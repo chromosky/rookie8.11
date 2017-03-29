05f00a1328ae7209c2168f3682a2e813cecaf569
JanWrotkowski <trojmiasto2@wp.pl>
2017-03-29 11:47:10 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
-//sprawdzam ile elementow li 
-function myFunction() {
-    var x = document.getElementsByTagName("li");
-    document.getElementById("btnNbr").innerHTML = x.length;
-}
 
 function function1() {
-  var ul = document.getElementById("ulList");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("x+1"));
-  li.setAttribute("id", "Item x+1"); // added line
-  ul.appendChild(li);
-  alert(li.id);
-}    
 
-var add = document.getElementById('js-addElem');
-console.log(add);
+    var ul = document.getElementById("lista");
+    ul.innerHTML += '<li>item</li>';
+};
 
-addElem.addEventListener('click', function(e) {
-e.target.innerHTML += '<li>item</li>';
-});
+addElem.addEventListener('click', function1);
+
+function myFunction() {
+    var x = document.getElementByTagName("li");
+    document.getElementById("liItem").innerHTML = x.length;
+}
+
